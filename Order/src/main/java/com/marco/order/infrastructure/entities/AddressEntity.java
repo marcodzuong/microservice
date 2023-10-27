dab2ce792b17409f9dfbeea83a203eaad0edf712
BachDV <bachdv@wiwatech.com>
2023-10-27 16:57:58 +0700

    add address

--- Order/src/main/java/com/marco/order/infrastructure/entities/AddressEntity.java
+++ Order/src/main/java/com/marco/order/infrastructure/entities/AddressEntity.java
@@ -8,11 +8,19 @@ public class AddressEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "city", length = 200, nullable = false)
+    private String city ;
 
-    private String City ;
-    private String Country ;
-    private String State ;
-    private String Street ;
-    private String ZipCode ;
+    @Column(name = "country", length = 200, nullable = false)
+    private String country ;
+
+    @Column(name = "state", length = 200, nullable = false)
+    private String state ;
+
+    @Column(name = "street", length = 200, nullable = false)
+    private String street ;
+
+    @Column(name = "zipCode", length = 200, nullable = false)
+    private String zipCode ;
 
 }
